0afa036401263fea1a407f84f6ed781f7540da5b
alexxasO <alexis.jouquan@epitech.eu>
2024-12-05 09:51:20 +0100

    feat(start, scripts): enhance start script to create .env file if missing and add wait-for-it functionality; update create_indexers script to check for jq dependency

--- scripts/prowlarr/create_indexers.sh
+++ scripts/prowlarr/create_indexers.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
+# Check for dependencies
+if ! command -v jq &> /dev/null; then
+  echo "jq could not be found, please install it"
+  exit 1
+fi
+
 set -a && source .env && set +a
 
-PROWLARR_API_KEY="be3bd470054a415fbe8dc0e97b5ce1ac"
+PROWLARR_API_KEY="06655a1a79174e73be6c3f2ec18fd02e"
+PASSKEY_SHAREWOOD="4c8ad0b1ab0ab979064bb474cf0e55b9 "
 
 # Sharewood Indexer
+jq --arg passkey "$PASSKEY_SHAREWOOD" '
+  .fields |= map(
+    if .name == "passkey" then
+      .value = $passkey
+    else
+      .
+    end
+)' "${SCRIPTS_PATH}/prowlarr/indexers/sharewood.json" | \
 curl -X POST "http://prowlarr.lan/api/v1/indexer" \
 -H "X-Api-Key: ${PROWLARR_API_KEY}" \
 -H "Content-Type: application/json" \
--d '{
-  "name": "Sharewood",
-  "enable": true,
-  "protocol": "usenet",
-  "implementationName": "Sharewood",
-  "implementation": "Sharewood",
-  "configContract": "SharewoodSettings",
-  "fields": [
-    {
-        "name": "baseUrl",
-        "value": "https://api.sharewood.com"
-    },
-    {
-        "name": "apiKey",
-        "value": "4c8ad0b1ab0ab979064bb474cf0e55b9"
-    }
-  ]
-}'
+-d @-
+
+# 1337x Indexer
+curl -X POST "http://prowlarr.lan/api/v1/indexer" \
+-H "X-Api-Key: ${PROWLARR_API_KEY}" \
+-H "Content-Type: application/json" \
+-d @${SCRIPTS_PATH}/prowlarr/indexers/1337x.json
--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
-# launch services
-docker compose up -d
+if [ ! -f .env ]; then
+    echo "Creating .env file"
+    cp .env.example .env
+fi
+
+./wait-for-it.sh localhost:9696 -- echo "Prowlarr is up"
+./wait-for-it.sh localhost:8989 -- echo "Sonarr is up"
+
+# launch services if arg script-only is not passed
+if [ "$1" != "--scripts-only" ]; then
+    echo "Launching services"
+    docker compose up -d
+fi
 
 # execute all scripts recursively in scripts folder
 for script in $(find ./scripts -type f -name "*.sh" | sort); do
