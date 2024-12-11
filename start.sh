7e0446be3f2af23ac505e44221eb512b13ebd0d1
alexxasO <alexis.jouquan@epitech.eu>
2024-12-11 16:28:03 +0100

    feat(jellyfin): enhance Dockerfile with custom HTML scripts for UI updates; add socket-proxy configuration for improved security

--- start.sh
+++ start.sh
@@ -3,14 +3,14 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-./wait-for-it.sh localhost:9696 -- echo "Prowlarr is up"
-./wait-for-it.sh localhost:8989 -- echo "Sonarr is up"
-
 # launch services if arg script-only is not passed
 if [ "$1" != "--scripts-only" ]; then
     echo "Launching services"
-    docker compose up -d
+    docker compose --profile blackhole up -d
 fi
+
+./wait-for-it.sh localhost:9696 -- echo "Prowlarr is up"
+./wait-for-it.sh localhost:8989 -- echo "Sonarr is up"
 
 # execute all scripts recursively in scripts folder
 for script in $(find ./scripts -type f -name "*.sh" | sort); do
